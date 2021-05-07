c7410a8e3731db5d2a97a7892f7dd32650a56823
Michal Young <michal.young@gmail.com>
2021-05-07 15:55:38 -0700

    Factor unit productions: if <A> ::= <B> or <A> ::= 'a', substitute <B> or 'a' for <A> everywhere it appears, then remove <A> from the grammar (unless <A> is the root).

--- demo_gen.py
+++ demo_gen.py
@@ -1,29 +1,37 @@
 """Glue together parsing and random sentence generation"""
 
-from pygramm.grammar import FactorEmpty
 from pygramm.llparse import *
 from pygramm.generator import *
-from pygramm.binary_choice import Binary_Choices
+from pygramm.unit_productions import UnitProductions
 
-# f = open("data/english.txt")
-# gram = parse(f)
-# xform = FactorEmpty(gram)
-# xform.transform_all_rhs(gram)
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
-f = open("data/simple_seq.txt")
+# f = open("data/gram-wf-original-glade.txt")
+f = open("data/gram-calc-multi-line-2020-06-04_22-59.gram.txt")
 gram = parse(f)
+gram.finalize()
+xform = UnitProductions(gram)
+xform.transform_all_rhs(gram)
+
+
 log.debug(f"Grammar is {gram}")
 
 # xform = grammar.FactorEmpty(gram)
 # xform.transform_all_rhs(gram)
 
-binarize = Binary_Choices(gram)
-binarize.transform_all_rhs(gram)
+# binarize = Binary_Choices(gram)
+# binarize.transform_all_rhs(gram)
 
 print("*** Grammar (repr): ***")
 for name in gram.symbols:
     sym = gram.symbols[name]
     print(f"{sym} ::= {repr(sym.expansions)}")
 print("*** *** ***")
+print("*** Grammar (str) ***")
+print(gram.dump())
+print("*** Generated sentence ***")
 # budget = max(5, 2 * gram.start.min_tokens())
 random_sentence(gram, budget=20, min_length=15)
