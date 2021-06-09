1d6eae0e50216f1ea01e20c8dd2bbe8f6cdbc32f
Michal Young <michal.young@gmail.com>
2021-06-08 20:52:04 -0700

    Simplified --- there is only the bigram version. May have also repaired a performance bug (unnecessary initialization steps because I forgot to set "forked" to True in the fork call).

--- biased_choice.py
+++ biased_choice.py
@@ -5,79 +5,73 @@ and biases future choices toward those biases.
 import random
 from typing import List
 
+# Tuning constants
+#
+# The weight that all items start at.  Must be in open interval
+# 0.0 .. 1.0.  I can't think of a reason that this would change.
+DEFAULT_WEIGHT = 0.5
+#
+# Default learning rates.  Move current weight what fraction of the way from its
+# current value toward 1.0 (if reward) or 0.0 (if penalty).  Small values will learn
+# slowly, large values will oscillate.  If rewards are rare, we might want a penalty
+# delta that is smaller than the reward delta.
+REWARD_DELTA = 0.05
+PENALTY_DELTA = 0.05
+#
+# If we have a bigram xa, and we also have a weight for a regardless of
+# prior, how much of the weight value should depend on the bigram weight?
+# Since we see individual items more often than we see bigrams, this should
+# probably not be 1.0.
+BIGRAM_PRIORITY = 0.8
+
 class _BiasCore:
     """Core shared state of the biased chooser.  When a chooser is
-    forked, we keep a reference to this part.
+    forked, we keep a reference to this part (i.e., it is shared
+    among all Bias objects forked from an initial Bias object).
     """
 
-    def __init__(self, default_weight=0.5, initial_weights={},
-                 reward_delta=0.05, penalty_delta=0.05):
+    def __init__(self,
+                 default_weight=DEFAULT_WEIGHT,
+                 reward_delta=REWARD_DELTA,
+                 penalty_delta=PENALTY_DELTA,
+                 bigram_priority = BIGRAM_PRIORITY):
         assert 0.0 < default_weight < 1.0, "default weights must be in open interval 0.0..1.0"
         self.default_weight = default_weight
-        self.weights = initial_weights.copy()
         assert 0.0 < reward_delta < 1.0
-        assert 0.0 < penalty_delta < 1.0
         self.reward_delta = reward_delta
+        assert 0.0 < penalty_delta < 1.0
         self.penalty_delta = penalty_delta
+        self.bigram_priority = bigram_priority
+        self.weights = {}
+        self.bigram_weights = {}
 
-    def weight(self, item: object, context=None) -> float:
-        """Current weight of an item, initialized if needed.
-        (Context ignored in this version, but can be overridden
-        in a subclass.)
-        """
-        if item not in self.weights:
-            self.weights[item] = self.default_weight
-        return self.weights[item]
-
-    def choose(self, choices: List[object], context=None):
+    def choose(self, choices: List[object], prior=None):
         """Make a biased choice among choices."""
         if not choices:
             return None
-        sum_weight = sum(self.weight(item, context) for item in choices)
+        sum_weight = sum(self.weight(item, prior) for item in choices)
         r = random.random()  # In open interval 0.0 .. 1.0
         bound = 0.0  # Sum of adjusted weights so far
         for choice in choices:
-            bound += self.weight(choice, context) / sum_weight
+            bound += self.weight(choice, prior) / sum_weight
             if r <= bound:
                 return choice
         # Infinitessimal possibility of roundoff error
         return choices[-1]
 
-    def reward(self, item: object):
-        """Choose this one more often"""
-        old_weight = self.weight(item)
-        new_weight = old_weight + self.reward_delta * (1.0 - old_weight)
-        self.weights[item] = new_weight
-
-    def penalize(self, item: object):
-        """Choose this one less often"""
-        old_weight = self.weight(item)
-        new_weight = old_weight - self.penalty_delta * old_weight
-        self.weights[item] = new_weight
-
-
-class _BigramCore(_BiasCore):
-    """Biased chooser core that keeps track of bigrams
-    in addition to individual items.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.bigram_weights = {}
-        # If we have a recorded preference for a bigram, how
-        # should we combine it with the weight for the individual
-        # item?
-        self.bigram_priority = 0.9
-
-    def weight(self, item: object, context=None) -> float:
+    def weight(self, item: object, prior=None) -> float:
         """Current weight of an item, initialized if needed."""
-        bigram = (context, item)
+        bigram = (prior, item)
         if item not in self.weights:
             self.weights[item] = self.default_weight
         item_weight = self.weights[item]
         if bigram not in self.bigram_weights:
+            # Haven't seen it in this context; depend on its
+            # overall weight from all contexts in which we've seen it.
             return self.weights[item]
         bi_weight = self.bigram_weights[bigram]
         return self.bigram_priority * bi_weight + (1 - self.bigram_priority) * item_weight
+
 
     def reward(self, item: object, prior: object=None):
         """Choose this one / this pair more often"""
@@ -111,27 +105,28 @@ class _BigramCore(_BiasCore):
         new_weight = old_weight - self.penalty_delta * old_weight
         self.bigram_weights[bigram] = new_weight
 
+
 class Bias:
     """Provides a 'choice' method like random.choice but weighted.
     Choices must be hashable.
     """
 
-    def __init__(self, default_weight=0.5, initial_weights={},
-                 reward_delta=0.05, penalty_delta=0.05, forked=False):
+
+    def __init__(self,
+                default_weight = DEFAULT_WEIGHT,
+                reward_delta = REWARD_DELTA,
+                penalty_delta = PENALTY_DELTA,
+                bigram_priority = BIGRAM_PRIORITY,
+                forked=False):
         if not forked:
-            self.core = _BiasCore(default_weight, initial_weights,
-                                  reward_delta, penalty_delta)
+            self.core = _BiasCore(default_weight,  reward_delta, penalty_delta, bigram_priority)
             self.history = []
 
     def fork(self) -> 'Bias':
-        forked = Bias()
+        forked = Bias(forked=True)
         forked.core = self.core
         forked.history = self.history.copy()
         return forked
-
-    def weight(self, item: object) -> float:
-        """Current weight of an item, initialized if needed."""
-        return self.core.weight(item)
 
     def choose(self, choices: List[object]):
         """Make a biased choice among choices."""
@@ -140,60 +135,19 @@ class Bias:
         return choice
 
     def reward(self):
-        """These were good choices"""
-        for item in self.history:
-            self.core.reward(item)
-
-    def penalize(self):
-        """Choose this one less often"""
-        for item in self.history:
-            self.core.penalize(item)
-
-
-class BigramBias(Bias):
-    """Provides a 'choice' method like random.choice but weighted.
-    Choices must be hashable.  Bigrams:  Context is one prior item. 
-    """
-
-    def __init__(self, default_weight=0.5, initial_weights={},
-                 reward_delta=0.05, penalty_delta=0.05, forked=False):
-        if not forked:
-            self.core = _BigramCore(default_weight, initial_weights,
-                                  reward_delta, penalty_delta)
-            self.history = []
-
-    def fork(self) -> 'BigramBias':
-        forked = BigramBias()
-        forked.core = self.core
-        forked.history = self.history.copy()
-        return forked
-
-    def weight(self, item: object, context=None) -> float:
-        """Current weight of an item, initialized if needed."""
-        return self.core.weight(item, context)
-
-    def choose(self, choices: List[object]):
-        """Make a biased choice among choices."""
-        prior = None
-        if self.history:
-            prior = self.history[-1]
-        choice = self.core.choose(choices, prior)
-        self.history.append(choice)
-        return choice
-
-    def reward(self):
-        """These were good choices"""
+        """These were good choices; make them more often"""
         prior = None
         for item in self.history:
             self.core.reward(item, prior=prior)
             prior = item
 
     def penalize(self):
-        """Choose this one less often"""
+        """These were not good choices; avoid them"""
         prior = None
         for item in self.history:
             self.core.penalize(item, prior=prior)
             prior = item
+
 
 
 def main():
@@ -202,8 +156,7 @@ def main():
     a few hundred iterations.
     """
     letters = list("abcdefghijklmnopqrstuvwxyz")
-    #chooser = Bias(reward_delta=0.25, penalty_delta=0.25)
-    root_chooser = BigramBias()
+    root_chooser = Bias()
     for epoch in range(100):
         for word in range(1000):
             chooser = root_chooser.fork()
